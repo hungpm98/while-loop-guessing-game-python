98714ac1394f8eaef7a405b9fc67866927b5ca56
unknown <hungpm.works@gmail.com>
2022-10-05 03:23:07 +0700

    first update

---
+++ work.py
@@ -0,0 +1,20 @@
+from re import A
+
+
+secret_word = "mashallah"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guess = False
+while guess != secret_word and not(out_of_guess):
+    if guess_count < guess_limit:
+        guess = input("Enter your guess: ")
+        guess_count += 1
+    else:
+        out_of_guess = True
+
+if out_of_guess:
+    print("Out of guess, you Lose!")
+else:
+    print("Congrat, you win!")
+
